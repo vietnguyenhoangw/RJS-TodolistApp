509ea04ae3ccc3e2becf81e9324fb9d247785175
vietnguyenhoangw <hoang-viet.nguyen@ant-tech.eu>
2020-11-03 00:37:14 +0700

    First commit - part8. render list :rocket:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TodoItem from "./components/TodoItem";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      "Quánh golf",
+      "Đá bóng",
+      "Đá bóng",
+      "Diễn xiếc",
+      "Coi phim",
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.todoItems.map((item, index) => (
+            <TodoItem title={item} key={index} />
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
